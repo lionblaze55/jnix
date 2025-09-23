fde3212d6a0b222965fc7b4bbd91333195159d80
lionblaze55 <joshfaber.g@gmail.com>
2025-09-23 11:45:12 -0400

    Update flake.nix
    more dehomemanagerification

--- flake.nix
+++ flake.nix
@@ -3,10 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # TODO removing home manager for now
+    #home-manager = {
+    #  url = "github:nix-community/home-manager";
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #};
   };
 
   outputs = { self, nixpkgs, home-manager, ... }: {
@@ -16,35 +17,31 @@
         system = "x86_64-linux";
         modules = [
           ./systems/jdesktop/jdesktop.nix
-          home-manager.nixosModules.home-manager
+          # home-manager.nixosModules.home-manager
         ];
       };
       "jlaptop" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./systems/jlaptop/jlaptop.nix
-          home-manager.nixosModules.home-manager
         ];
       };
       "edesktop" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./systems/edesktop/edesktop.nix
-          home-manager.nixosModules.home-manager
         ];
       };
       "elaptop" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./systems/elaptop/elaptop.nix
-          home-manager.nixosModules.home-manager
         ];
       };
       "jtv" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./systems/jtv/jtv.nix
-          home-manager.nixosModules.home-manager
         ];
       };
     };
