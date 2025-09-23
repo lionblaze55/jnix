b7777b7a5f2e3939c3f669ee3d13ba96637d9199
lionblaze55 <joshfaber.g@gmail.com>
2025-09-23 11:36:51 -0400

    Update jlaptop.nix
    removed homemanager from active config

--- systems/jlaptop/jlaptop.nix
+++ systems/jlaptop/jlaptop.nix
@@ -17,17 +17,17 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  # Home Manager configuration for the user
-  home-manager.users.jfaber = {
-    home.stateVersion = "25.05";
-    imports = [
-      ./../modules/jfaber-common.nix # A shared module
-      #./homemanager/discord.nix     # Host-specific sub-config for Discord
-      ./homemanager/sway.nix        # Host-specific sub-config for Sway
-      ./homemanager/waybar.nix        # Host-specific sub-config for waybar
-      # Add other host-specific Home Manager configs here
-    ];
-  };
+  # #TODO removed until understood better! Home Manager configuration for the user
+  #home-manager.users.jfaber = {
+  #  home.stateVersion = "25.05";
+  #  imports = [
+  #    ./../modules/jfaber-common.nix # A shared module
+  #    #./homemanager/discord.nix     # Host-specific sub-config for Discord
+  #    ./homemanager/sway.nix        # Host-specific sub-config for Sway
+  #    ./homemanager/waybar.nix        # Host-specific sub-config for waybar
+  #    # Add other host-specific Home Manager configs here
+  #  ];
+  #};
 
   # ... (other system configurations)
 }
