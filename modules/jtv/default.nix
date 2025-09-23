32c1709588a6b9f76cb05b39e50f4e8aa2e4109c
lionblaze55 <joshfaber.g@gmail.com>
2025-09-23 15:01:29 -0400

    Create default.nix

---
+++ modules/jtv/default.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+
+{
+  # Define the generic 'user'.
+  users.users.user = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+  };
+
+  # Enable the XFCE desktop environment.
+  #services.xserver = {
+  #  enable = true;
+  #  desktopManager.xfce.enable = true;
+  #  displayManager.lightdm.enable = true;
+  #};
+
+  # TV-specific applications.
+  environment.systemPackages = with pkgs; [
+    mpv
+    plex
+    vlc
+  ];
+}
