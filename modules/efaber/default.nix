524a8518db2156044546f10f82334ea5057f0136
Joshua Faber <joshfaber.g@gmail.com>
2025-09-24 19:45:30 -0400

    added new flatpak config for both users.

--- modules/efaber/default.nix
+++ modules/efaber/default.nix
@@ -5,11 +5,24 @@
   users.users.efaber = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
-  ];
+  };
+
+  services.flatpak.enable = true;
+
+  # Replace "your_username" with your actual username
+  users.users.efaber = {
+    packages = with pkgs; [
+      flatpak
+    ];
+    # This line sets up a user-level activation script
+    # that will run the flatpak remote-add command.
+    extraActivation = ''
+      ${pkgs.flatpak}/bin/flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
 
   # Common applications for efaber.
   environment.systemPackages = with pkgs; [
-    brave
-    libreoffice
+    krita
   ];
 }
--- modules/jfaber/default.nix
+++ modules/jfaber/default.nix
@@ -13,15 +13,27 @@
     wrapperFeatures.gtk = true;
   };
 
-  # Enable vscodium
-  environment.systemPackages = with pkgs; [ vscodium ];
-
   services.gnome.gnome-keyring.enable = true;
+
+  services.flatpak.enable = true;
+
+  # Replace "your_username" with your actual username
+  users.users.jfaber = {
+    packages = with pkgs; [
+      flatpak
+    ];
+    # This line sets up a user-level activation script
+    # that will run the flatpak remote-add command.
+    extraActivation = ''
+      ${pkgs.flatpak}/bin/flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
 
   # Common applications for jfaber.
   environment.systemPackages = with pkgs; [
     firefox
     grimshot
+    vscodium
     #vscode
     #element-desktop
     #obs-studio
