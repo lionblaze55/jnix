58ea83c829069aff51251f91d0a842f4f2dc9c7f
lionblaze55 <joshfaber.g@gmail.com>
2025-09-25 10:51:15 -0400

    Update configuration.nix

--- systems/jlaptop/configuration.nix
+++ systems/jlaptop/configuration.nix
@@ -25,12 +25,6 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "jlaptop"; # Define your hostname.
-#  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-#  networking.wireless.networks = {
-#    fLink_2G = {
-#      psk = "I<3Millie";
-#    };
-#  };
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
