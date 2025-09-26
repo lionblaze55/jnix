98c61741e56a9b3fb3134a4fc3ce444cde41f10b
Joshua Faber <joshfaber.g@gmail.com>
2025-09-25 22:12:44 -0400

    more typo

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -30,7 +30,7 @@
   programs.gamemode.enable = true;
 
   #PPD
-  services.power-profiles-daemon.enable;
+  services.power-profiles-daemon.enable=true;
 
   # Enable zram swapping on all machines
   zramSwap = {
