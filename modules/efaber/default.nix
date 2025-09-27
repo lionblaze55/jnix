533109df553b7a1bea3ed3829fbd8c4923c463e4
Joshua Faber <joshfaber.g@gmail.com>
2025-09-27 01:22:17 -0400

    modified kde stuff for embil

--- modules/efaber/default.nix
+++ modules/efaber/default.nix
@@ -12,19 +12,20 @@
   # Common applications for efaber.
   environment.systemPackages = with pkgs; [
     krita
-    konsole
+    kdePackages.konsole
+    kdePackages.kate
   ];
 
   # Enable KDE for embil
   services.xserver.enable = true; # optional
-  services.displayManager.sddm.enable = true;
-  #services.displayManager.sddm.wayland.enable = true;
+  services.displayManager = {
+    sddm.wayland.enable = true;
+    autologin.enable = true;
+    autologin.user = efaber;
   services.desktopManager.plasma6.enable = true;
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     plasma-browser-integration
-    konsole
-    elisa
   ];
   services.displayManager.sddm.wayland.enable = true;
   hardware.bluetooth.enable = true;
