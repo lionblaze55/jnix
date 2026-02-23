0ed576b556fd9576a81fd80007f9185b90b1a775
lionblaze55 <joshfaber.g@gmail.com>
2026-02-23 14:35:18 -0500

    Update configuration.nix

--- systems/emacbook/configuration.nix
+++ systems/emacbook/configuration.nix
@@ -118,5 +118,5 @@
   #  ];
   #};
   # ... (other system configurations)
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "25.11"; # Did you read the comment?
 }
